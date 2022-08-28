569750196615f73b93bb044784ac760ed961af6b
Li Jiawen <lijiawen0897@outlook.com>
2022-08-29 01:29:32 +0800

    Add files via upload

--- public_wsi_process/public_cut_tiles.py
+++ public_wsi_process/public_cut_tiles.py
@@ -9,11 +9,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def cut_tiles(slide_path, h5_path, save_h5_to_tiles_path):
-    slide_id = slide_path.split('\\')[-1].split('.')[0]
-    # h5_to_file_path = os.path.join(os.path.dirname(slide_dir), 'patches', '{}.h5'.format(slide_id))
-    # file_to_save_path = os.path.join(save_path, '{}.png'.format(slide_id))
-
+def cut_tiles(slide_path, h5_path, save_h5_to_tiles_path, slide_id):
     wsi = openslide.open_slide(slide_path)
     with h5py.File(h5_path, 'r') as hdf5_file:
         patch_level = hdf5_file['coords'].attrs['patch_level']
@@ -31,9 +27,13 @@ def cut_tiles(slide_path, h5_path, save_h5_to_tiles_path):
 
 
 parser = argparse.ArgumentParser(description='Feature Extraction')
-parser.add_argument('--slide_dir', type=str, default='D:/LocalPC_PythonProject/openslide_demo/img/tif/slides')
-parser.add_argument('--h5_dir', type=str, default='D:/LocalPC_PythonProject/openslide_demo/img/tif/h5')
-parser.add_argument('--save_dir', type=str, default='D:/LocalPC_PythonProject/openslide_demo/img/tif')
+parser.add_argument('--slide_dir', type=str, #default='D:/LocalPC_PythonProject/openslide_demo/img/tif/slides'
+                    )
+parser.add_argument('--h5_dir', type=str, #default='D:/LocalPC_PythonProject/openslide_demo/img/tif/h5'
+                    )
+parser.add_argument('--save_dir', type=str, #default='D:/LocalPC_PythonProject/openslide_demo/img/tif'
+                    )
+parser.add_argument('--sys_select', type=str, default=None, help='--win, windows system; --linux, linux system')
 args = parser.parse_args()
 
 if __name__ == '__main__':
@@ -47,7 +47,10 @@ if __name__ == '__main__':
     slide_path_list = glob.glob(os.path.join(slide_dir, '*.tif'))
     i = 1
     for slide_path in slide_path_list:
-        slide_id = slide_path.split('\\')[-1].split('.')[0]
+        if args.sys_select == 'win':
+            slide_id = slide_path.split('\\')[-1].split('.')[0]
+        elif args.sys_select == 'linux':
+            slide_id = slide_path.split('/')[-1].split('.')[0]
         h5_path = os.path.join(h5_dir, '{}.h5'.format(slide_id))
         save_h5_to_tiles_path = os.path.join(save_dir, slide_id)
         print('progress: {:.2%}, {}/{} \t processing {}'.format(i/len(slide_path_list), i, len(slide_path_list),
@@ -59,7 +62,7 @@ if __name__ == '__main__':
         else:
             os.mkdir(save_h5_to_tiles_path)
             i += 1
-            cut_tiles(slide_path=slide_path, h5_path=h5_path, save_h5_to_tiles_path=save_h5_to_tiles_path)
+            cut_tiles(slide_path=slide_path, h5_path=h5_path, save_h5_to_tiles_path=save_h5_to_tiles_path, slide_id=slide_id)
             print('{} is finished'.format(slide_id))
 
 
