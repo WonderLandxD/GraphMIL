65c5bacddc00be8646961515dca7f8e5e07b43dc
Li Jiawen <lijiawen0897@outlook.com>
2022-09-05 01:53:13 +0800

    Updating 4-fold cross validation

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ from util import AverageMeter
 from tqdm import tqdm
 import torch.utils.data as Dataloader
 from torch.utils.data import Dataset
+from torch.utils.data import ConcatDataset
 from feature_vig_backbone import GraphMILBackbone
 from random import sample
 
@@ -33,7 +34,7 @@ class SYSDataset(Dataset):
     def __len__(self):
         return(len(self.feat_list))
 
-def model_train(model, train_loader, criterion, optimizer, epoch):
+def model_train(model, train_loader, criterion, optimizer, idx):
     model.train()
 
     batch_time = AverageMeter()
@@ -69,15 +70,19 @@ def model_train(model, train_loader, criterion, optimizer, epoch):
         batch_time.update(time.time() - end)
         end = time.time()
 
+    print('Train ({}-fold):\t'
+          'Average Loss {loss.avg:.3f}\t'
+          'Average Acc {acc.avg:.3f}'.format(idx+1, loss=losses, acc=acc))
+
         # print statistics and write summary every N batch
-        if (batch_idx + 1) % len(train_loader) == 0:
-            print('{}'.format("GraphMIL"))
-            print('Train: [{0}][{1}/{2}]\t'
-                  'BT {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'loss {loss.val:.3f} ({loss.avg:.3f})\t'
-                  'acc {acc.val:.3f} ({acc.avg:.3f})'.format(
-                epoch, batch_idx + 1, len(train_loader), batch_time=batch_time, loss=losses,
-                acc=acc))
+        # if (batch_idx + 1) % len(train_loader) == 0:
+        #     print('{}'.format("GraphMIL"))
+        #     print('Train: [{0}][{1}/{2}]\t'
+        #           'BT {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+        #           'loss {loss.val:.3f} ({loss.avg:.3f})\t'
+        #           'acc {acc.val:.3f} ({acc.avg:.3f})'.format(
+        #         epoch, batch_idx + 1, len(train_loader), batch_time=batch_time, loss=losses,
+        #         acc=acc))
 
     return losses.avg, acc.avg
 
@@ -115,15 +120,19 @@ def model_val(model, val_loader, criterion, epoch):
             batch_time.update(time.time() - end)
             end = time.time()
 
+        print('Valid ({}-fold):\t'
+              'Average Loss {loss.avg:.3f}\t'
+              'Average Acc {acc.avg:.3f}'.format(idx+1, loss=losses, acc=acc))
+
             # print statistics and write summary every N batch
-            if (batch_idx + 1) % len(val_loader) == 0:
-                print('{}'.format('GraphMIL'))
-                print('Val: [{0}][{1}/{2}]\t'
-                      'BT {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                      'loss {loss.val:.3f} ({loss.avg:.3f})\t'
-                      'acc {acc.val:.3f} ({acc.avg:.3f})'.format(
-                    epoch, batch_idx + 1, len(val_loader), batch_time=batch_time, loss=losses,
-                    acc=acc))
+            # if (batch_idx + 1) % len(val_loader) == 0:
+            #     print('{}'.format('GraphMIL'))
+            #     print('Val: [{0}][{1}/{2}]\t'
+            #           'BT {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+            #           'loss {loss.val:.3f} ({loss.avg:.3f})\t'
+            #           'acc {acc.val:.3f} ({acc.avg:.3f})'.format(
+            #         epoch, batch_idx + 1, len(val_loader), batch_time=batch_time, loss=losses,
+            #         acc=acc))
 
     return losses.avg, acc.avg
 
@@ -131,11 +140,11 @@ def parse_args():
 
     parser = argparse.ArgumentParser('Argument for training')
 
-    parser.add_argument('--gpu', type=str, default='4', help='GPU Setting, default=0')
-    parser.add_argument('--num_epoch', type=int, default=1000, help='NUMBER OF EPOCH, default=100')
+    parser.add_argument('--gpu', type=str, default='4', help='GPU Setting, default=4')
+    parser.add_argument('--num_epoch', type=int, default=100, help='NUMBER OF EPOCH, default=100')
     parser.add_argument('--lr', default=0.0001, type=float, help='LEARNING RATE, default=0.0001')
     parser.add_argument('--dataset', default='SYS', type=str, help='DATASET, default=SYS')
-    parser.add_argument('--upper_bound', type=int, default=8000, help='SAMPLER NUMBER, default=8000')
+    parser.add_argument('--upper_bound', type=int, default=8000, help='Upper bound on the number of tiles per WSI, default=8000')
 
     args = parser.parse_args()
 
@@ -155,86 +164,107 @@ if __name__ == '__main__':
 
         full_dataset = SYSDataset(DATA_ROOT)
 
-        train_size = int(0.7 * len(full_dataset))
-        valid_size = len(full_dataset) - train_size
 
-        train_dataset, valid_dataset = torch.utils.data.random_split(full_dataset, [train_size, valid_size])
+        #### 4-fold cross validation
+        k = 4  # Can not change for now
+        ratio = 1 / 4
 
-        train_loader = Dataloader.DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=0)
-        valid_loader = Dataloader.DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=0)
+        size_1 = int(ratio * len(full_dataset))
+        size_2 = len(full_dataset) - size_1 * (k-1)
 
-        ### Save log ###
-        # save_log = './SYS_Results/Cross_Validation'
-        _save_folder = './SYS_Results/DemoTrainVal'
-        for i in range(1, 100):
-            save_folder = '{}_{}'.format(_save_folder, i)
-            if os.path.exists(save_folder):
-                i += 1
+        dataset_0, dataset_1, dataset_2, dataset_3 = torch.utils.data.random_split(full_dataset, [size_1, size_1, size_1, size_2])
+        dataset_list = [dataset_0, dataset_1, dataset_2, dataset_3]
+
+        _exp_log = './SYS_Results/4_fold_Cross_Validation'
+        for p in range(1, 100):
+            exp_log = '{}_{}'.format(_exp_log, p)
+            if os.path.exists(exp_log):
+                p += 1
             else:
-                os.makedirs(save_folder)
+                os.makedirs(exp_log)
                 break
 
-        n_data = len(train_dataset)
-        NUM_CLASSES = len(os.listdir(DATA_ROOT))
+        for idx in range(k):
+            print('----------{}-fold Cross Validation, No.{}-----------'.format(k, idx+1))
+            valid_dataset = dataset_list[idx]
+            train_list = list(set(dataset_list) - {dataset_list[idx]})
+            train_dataset = ConcatDataset(train_list)
 
-        print('---number of training samples: {}---'.format(n_data))
-        print('---number of training samples: {}---'.format(n_data),
-              file=open(os.path.join(save_folder, 'datasetting.txt'), 'w'))
-        n_data = len(valid_dataset)
-        print('---number of validation samples: {}---'.format(n_data))
-        print('---number of validation samples: {}---'.format(n_data),
-              file=open(os.path.join(save_folder, 'datasetting.txt'), 'a'))
+            train_loader = Dataloader.DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=0)
+            valid_loader = Dataloader.DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=0)
 
-        model = GraphMILBackbone(in_features=2048, num_classes=NUM_CLASSES, conv_class='edge',
-                                 drop_path=0.0, drop_out=0.0)
+            ### Save log ###
+            # save_log = './SYS_Results/Cross_Validation'
+            _save_folder = os.path.join(exp_log, 'exp')
+            for m in range(1, 100):
+                save_folder = '{}_{}'.format(_save_folder, m)
+                if os.path.exists(save_folder):
+                    m += 1
+                else:
+                    os.makedirs(save_folder)
+                    break
 
-        print('---Using {} Backbone---'.format('GraphMIL'))
+            n_data = len(train_dataset)
+            NUM_CLASSES = len(os.listdir(DATA_ROOT))
 
-        if torch.cuda.is_available() and torch.cuda.device_count() > 1:
-            model = torch.nn.DataParallel(model)
+            print('---number of training samples: {}---'.format(n_data))
+            print('---number of training samples: {}---'.format(n_data),
+                  file=open(os.path.join(save_folder, 'datasetting.txt'), 'w'))
+            n_data = len(valid_dataset)
+            print('---number of validation samples: {}---'.format(n_data))
+            print('---number of validation samples: {}---'.format(n_data),
+                  file=open(os.path.join(save_folder, 'datasetting.txt'), 'a'))
+
+            model = GraphMILBackbone(in_features=2048, num_classes=NUM_CLASSES, conv_class='edge',
+                                     drop_path=0.0, drop_out=0.0)
+
+            print('---Using {} Backbone---'.format('GraphMIL'))
+
+            if torch.cuda.is_available() and torch.cuda.device_count() > 1:
+                model = torch.nn.DataParallel(model)
 
 
-        criterion = nn.CrossEntropyLoss()
-        criterion = criterion.cuda()
+            criterion = nn.CrossEntropyLoss()
+            criterion = criterion.cuda()
 
-        if torch.cuda.is_available():
-            model = model.cuda()
+            if torch.cuda.is_available():
+                model = model.cuda()
 
-        optimizer = optim.Adam(model.parameters(), lr=args.lr)
+            optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
-        # Training Model
-        start_epoch = 1
-        pre_best_val_acc = float('-inf')
+            # Training Model
+            start_epoch = 1
+            pre_best_val_acc = float('-inf')
 
-        with open(os.path.join(save_folder, 'train_val_results.csv'), 'w') as f:
-            f.write('epoch, train_loss, train_acc, val_loss, val_acc\n')
+            with open(os.path.join(save_folder, 'train_val_results.csv'), 'w') as f:
+                f.write('epoch, train_loss, train_acc, val_loss, val_acc\n')
 
-        EPOCH = args.num_epoch
+            EPOCH = args.num_epoch
 
-        for epoch in range(start_epoch, EPOCH + 1):
+            for epoch in range(start_epoch, EPOCH + 1):
 
-            print('==> training...')
+                print('==> training...')
 
-            time_start = time.time()
+                time_start = time.time()
 
-            train_losses, train_acc = model_train(model, train_loader, criterion, optimizer, epoch)
-            print('Epoch time: {:.2f} s.'.format(time.time() - time_start))
+                train_losses, train_acc = model_train(model, train_loader, criterion, optimizer, idx)
+                print('Epoch time: {:.2f} s.'.format(time.time() - time_start))
 
-            print('==> validation...')
-            val_losses, val_acc = model_val(model, valid_loader, criterion, epoch)
+                print('==> validation...')
+                val_losses, val_acc = model_val(model, valid_loader, criterion, epoch)
 
-            # log results
-            with open(os.path.join(save_folder, 'train_val_results.csv'), 'a') as f:
-                f.write('%03d,%0.6f,%0.6f,%0.6f,%0.6f,\n' % (epoch, train_losses, train_acc, val_losses, val_acc))
+                # log results
+                with open(os.path.join(save_folder, 'train_val_results.csv'), 'a') as f:
+                    f.write('%03d,%0.6f,%0.6f,%0.6f,%0.6f,\n' % (epoch, train_losses, train_acc, val_losses, val_acc))
 
-            if (val_acc > pre_best_val_acc):
-                print('==> best acc Saving...')
-                torch.save(model.state_dict(), '{}/acc_best_model.pth'.format(save_folder))
-                pre_best_val_acc = val_acc
+                if (val_acc > pre_best_val_acc):
+                    print('==> best acc Saving...')
+                    torch.save(model.state_dict(), '{}/acc_best_model.pth'.format(save_folder))
+                    pre_best_val_acc = val_acc
 
-            if epoch % 10 == 0:
-                print('==> routine Saving...')
-                torch.save(model.state_dict(), '{}/epoch_{}_model.pth'.format(save_folder, epoch))
+                # if epoch % 10 == 0:
+                #     print('==> routine Saving...')
+                #     torch.save(model.state_dict(), '{}/epoch_{}_model.pth'.format(save_folder, epoch))
 
 
 
